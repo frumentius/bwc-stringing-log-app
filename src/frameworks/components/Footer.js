03020e11357ccc55c62fa0557dc4d1aabe271244
Frumentius Josopandojo <frumentius.josopandojo@paper.id>
2024-08-19 11:25:15 +1200

    feat: add link to footer

--- src/frameworks/components/Footer.js
+++ src/frameworks/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     let date_now = new Date();
   return (
     <div className="footer text-center py-4 md-typescale-body-medium">
-        &copy; {date_now.getFullYear()} Badminton Wellington Inc.
+        &copy; {date_now.getFullYear()} <a href="https://badminton.net.nz">Badminton Wellington Inc.</a>
         <br/>
         Developed by <a href="https://webtech.id" target="_blank" rel="noreferrer">Frumentius</a>
     </div>
