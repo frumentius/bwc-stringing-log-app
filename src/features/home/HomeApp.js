72cbf16e08a632efb4ddd663e2cf80b3e4d39163
Frumentius <frumentius_sonic@yahoo.com>
2024-08-23 14:57:57 +1200

    fix:manifest and fab position

--- src/features/home/HomeApp.js
+++ src/features/home/HomeApp.js
@@ -79,7 +79,7 @@ const HomeApp = () => {
           ></md-circular-progress>
         )}
       </div>
-      <div className="fixed right-4 bottom-4">
+      <div className="fixed right-6 bottom-6">
         <a
           href="https://docs.google.com/forms/d/e/1FAIpQLSf-eqigXjJJ9q2mtydiRA8fMQLGlr7uRDqb-ohXwfkIQxjhbQ/viewform?usp=sf_link"
           rel="noreferrer"
